a855302bc2c187f447fa0d0a112a1d29e6c98928
Timo Schmidt <timo.hund@yahoo.com>
2018-03-07 08:54:34 +0100

    [TASK] Split the templates into multiple templates with examples

--- Configuration/TCA/Overrides/sys_templates.php
+++ Configuration/TCA/Overrides/sys_templates.php
@@ -1,7 +1,26 @@
 <?php
 defined('TYPO3_MODE') or die();
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
     'solrfluidgrouping',
-    'Configuration/TypoScript/',
-    'Search - (Example) Fluid result grouping on type field'
+    'Configuration/TypoScript/Templates/',
+    'Search - Use templates from solrfluidgrouping'
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'solrfluidgrouping',
+    'Configuration/TypoScript/Examples/TypeFieldGroup/',
+    'Search - (Example) Fieldgroup on type field'
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'solrfluidgrouping',
+    'Configuration/TypoScript/Examples/PidQueryGroup/',
+    'Search - (Example) Querygroup on pid field'
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'solrfluidgrouping',
+    'Configuration/TypoScript/Examples/GroupedSuggest/',
+    'Search - (Example) Suggest with grouped results'
 );
