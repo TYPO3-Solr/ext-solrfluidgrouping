9617bada4964b063ceb6886c2ea13be804fed26d
Markus Friedrich <markus.friedrich@dkd.de>
2021-08-18 16:09:13 +0200

    [TASK] Release 10.0.0 (#21)
    Prepares the next version solrfluidgrouping 10.
    
    As we're planning to simplify the extension and dependency
    handling we're harmonize the extension version with TYPO3.
    So the next version of solrfluidgrouping is 10.0.0 and
    compatible with TYPO3 10 LTS.

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,32 +1,28 @@
 <?php
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
     'title' => 'Apache Solr for TYPO3 - Grouping for fluid rendering',
     'description' => 'This addon provides the grouping for the fluid templating',
-    'version' => '2.1.0',
+    'version' => '10.0.0',
     'state' => 'stable',
     'category' => 'plugin',
     'author' => 'Timo Hund, Frans Saris',
-    'author_email' => 'timo.hund@dkd.de',
+    'author_email' => 'solr-eb-support@dkd.de',
     'author_company' => 'dkd Internet Service GmbH',
-    'module' => '',
-    'uploadfolder' => 0,
-    'createDirs' => '',
-    'modify_tables' => '',
     'clearCacheOnLoad' => 0,
-    'constraints' => array(
-        'depends' => array(
+    'constraints' => [
+        'depends' => [
             'scheduler' => '',
-            'solr' => '9.0.0-',
-            'extbase' => '8.7.0-10.4.99',
-            'fluid' => '8.7.0-10.4.99',
-            'typo3' => '8.7.0-10.4.99'
-        ),
-        'conflicts' => array(),
-    ),
-    'autoload' => array(
-        'psr-4' => array(
+            'solr' => '11.1.0-',
+            'extbase' => '10.4.10-10.4.99',
+            'fluid' => '10.4.10-10.4.99',
+            'typo3' => '10.4.10-10.4.99'
+        ],
+        'conflicts' => [],
+    ],
+    'autoload' => [
+        'psr-4' => [
             'ApacheSolrForTypo3\\Solrfluidgrouping\\' => 'Classes/',
             'ApacheSolrForTypo3\\Solrfluidgrouping\\Tests\\' => 'Tests/'
-        )
-    )
-);
+        ]
+    ]
+];
